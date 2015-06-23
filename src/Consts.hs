30db9b786d7e1b399c0987febc8c765cc98f35c8
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-23 22:42:52 +0900

    separate Serieses

--- src/Consts.hs
+++ src/Consts.hs
@@ -1,7 +1,5 @@
 module Consts where
 import Haste.Graphics.Canvas
-import qualified Data.Map as M
-import Data.Complex
 
 defaultdθ :: Angle
 defaultdθ = 0.02
@@ -31,45 +29,3 @@ dw :: Int
 dw = 1
 pointSize :: Double
 pointSize = 1.0
-
-type Fourier = [Double]
-rectWave :: Fourier
-rectWave = [if even n then 0 else 1/fromIntegral n | n <- [1..]::[Int]]
-
-triangularWave :: Fourier
-triangularWave = [
-                 if even n
-                     then 0
-                     else 1/fromIntegral ((-1)^(n`div`2)*n^2) |
-                     n <- [1..]::[Int]
-                ]
-
-sawToothWave :: Fourier
-sawToothWave = [1 / fromIntegral n | n <- [1..]::[Int]]
-
-
-leafy :: Fourier
-leafy = [imagPart ((0:+1)^(n-1)) / (fromIntegral n* log (fromIntegral n))
-            | n <- [2..] :: [Int]]
-
-spiral :: Fourier
-spiral = [1 / (fromIntegral n*log(fromIntegral n)) | n <- [2..]::[Int]]
-
-play0 :: Fourier
-play0 = [ 0.2/ ((-1)^n *log (fromIntegral (n+1))) | n <- [1..] :: [Int]]
-
-play1 :: Fourier
-play1 = [if n `mod` 7 == 1
-            then  1 / fromIntegral n
-            else 0 | n <- [1..] :: [Int]]
-
-fouriers :: M.Map String Fourier
-fouriers = M.fromList [
-                      ("rectangular", rectWave),
-                      ("triangular", triangularWave),
-                      ("sawTooth", sawToothWave),
-                      ("spiral", spiral),
-                      ("leafy", leafy),
-                      ("play0", play0),
-                      ("play1", play1)
-                      ]
---
+++ src/Fouriers.hs
@@ -0,0 +1,44 @@
+module Fouriers where
+import Data.Complex
+import qualified Data.Map as M
+
+type Fourier = [Double]
+rectWave :: Fourier
+rectWave = [if even n then 0 else 1/fromIntegral n | n <- [1..]::[Int]]
+
+triangularWave :: Fourier
+triangularWave = [
+                 if even n
+                     then 0
+                     else 1/fromIntegral ((-1)^(n`div`2)*n^2) |
+                     n <- [1..]::[Int]
+                ]
+
+sawToothWave :: Fourier
+sawToothWave = [1 / fromIntegral n | n <- [1..]::[Int]]
+
+leafy :: Fourier
+leafy = [imagPart ((0:+1)^(n-1)) / (fromIntegral n* log (fromIntegral n))
+            | n <- [2..] :: [Int]]
+
+spiral :: Fourier
+spiral = [1 / (fromIntegral n*log(fromIntegral n)) | n <- [2..]::[Int]]
+
+play0 :: Fourier
+play0 = [ 0.2/ ((-1)^n *log (fromIntegral (n+1))) | n <- [1..] :: [Int]]
+
+play1 :: Fourier
+play1 = [if n `mod` 7 == 1
+            then  1 / fromIntegral n
+            else 0 | n <- [1..] :: [Int]]
+
+fouriers :: M.Map String Fourier
+fouriers = M.fromList [
+                      ("rectangular", rectWave),
+                      ("triangular", triangularWave),
+                      ("sawTooth", sawToothWave),
+                      ("spiral", spiral),
+                      ("leafy", leafy),
+                      ("play0", play0),
+                      ("play1", play1)
+                      ]
