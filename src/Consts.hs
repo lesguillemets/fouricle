f3b0a3905f816bf07d2757a8ad6decbf4a34bcda
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-23 22:38:05 +0900

    add functions

--- src/Consts.hs
+++ src/Consts.hs
@@ -1,6 +1,7 @@
 module Consts where
 import Haste.Graphics.Canvas
 import qualified Data.Map as M
+import Data.Complex
 
 defaultdθ :: Angle
 defaultdθ = 0.02
@@ -46,9 +47,29 @@ triangularWave = [
 sawToothWave :: Fourier
 sawToothWave = [1 / fromIntegral n | n <- [1..]::[Int]]
 
+
+leafy :: Fourier
+leafy = [imagPart ((0:+1)^(n-1)) / (fromIntegral n* log (fromIntegral n))
+            | n <- [2..] :: [Int]]
+
+spiral :: Fourier
+spiral = [1 / (fromIntegral n*log(fromIntegral n)) | n <- [2..]::[Int]]
+
+play0 :: Fourier
+play0 = [ 0.2/ ((-1)^n *log (fromIntegral (n+1))) | n <- [1..] :: [Int]]
+
+play1 :: Fourier
+play1 = [if n `mod` 7 == 1
+            then  1 / fromIntegral n
+            else 0 | n <- [1..] :: [Int]]
+
 fouriers :: M.Map String Fourier
 fouriers = M.fromList [
                       ("rectangular", rectWave),
                       ("triangular", triangularWave),
-                      ("sawTooth", sawToothWave)
+                      ("sawTooth", sawToothWave),
+                      ("spiral", spiral),
+                      ("leafy", leafy),
+                      ("play0", play0),
+                      ("play1", play1)
                       ]
